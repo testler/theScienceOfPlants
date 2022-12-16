1cc4297ba6b234b783a5943b1f17a284128ff93e
testler <joshgarst@me.com>
2022-12-15 23:54:12 -0600

    testing phase

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import * as THREE from "three";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const canvas = document.getElementById("bg");
+  const renderer = new THREE.WebGLRenderer({canvas});
+
+  renderer.setPixelRatio( window.devicePixelRatio);
+  renderer.setSize( window.innerWidth, window.innerHeight);
+  camera.position.setZ(30);
+ 
+  const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16);
+  const material = new THREE.MeshStandardMaterial({color:0x00ff00});
+  const box = new THREE.Mesh(geometry, material);
+  scene.add(box);
+
+  const light = new THREE.PointLight(0xffffff);
+  light.position.set(0,0, 15);
+  scene.add(light);
+  const lightHelp = new THREE.SpotLightHelper( light, 5);
+  scene.add(lightHelp);
+  
+  function animate() {
+    requestAnimationFrame( animate );
+    box.rotation.x += 0.05;
+    renderer.render( scene, camera);
+  }
+
+  animate();
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h1>hello?</h1>
     </div>
   )
 }
